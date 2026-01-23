9c4ce6666bd316dde827b96bfbbd21b4ab299d2f
moritzsoelderer <moritzsoelderer@gmail.com>
2026-01-23 16:22:37 +0100

    feat: implement with_where_clause method to use without providing explicit gql strings + create test cases (nested objects are still missing)

--- src/api/gql_dynamic_query_builder.py
+++ src/api/gql_dynamic_query_builder.py
@@ -1,6 +1,17 @@
 from __future__ import annotations
 
+import json
+
 from src.core.grammar.query import prepare_query_grammar
+
+
+def create_key_value_pair(value: str | int | list, operation: str) -> str:
+    if operation in ['_like', '_ilike']:
+        value = f'"%{value}%"'
+    elif isinstance(value, str):
+        value = f'"{value}"'
+
+    return f'{operation}: {value}'
 
 
 class GQLDynamicQueryBuilder:
@@ -10,9 +21,23 @@ class GQLDynamicQueryBuilder:
         self.processed_query = query
 
     def with_where_clause(
-        self, field_name: str, value: str | int | list, operation: str | list[str]
+        self, table_name: str, field_name: str, value: str | int | list[int] | list[str], operation: str | list[str]
     ):
-        pass
+        if isinstance(value, list):
+            if isinstance(operation, list):
+                pairs = [create_key_value_pair(v, o) for v, o in zip(value, operation, strict=True)]
+                self.where_clauses[table_name] = f'{field_name}: {{{" ".join(pairs)}}}'
+            else:
+                value = json.dumps(value)
+                self.where_clauses[table_name] = f'{field_name}: {{{operation}: {value}}}'
+        else:
+            if isinstance(operation, list):
+                raise TypeError("Operation should be scalar if value is scalar")
+
+            self.where_clauses[table_name] = f'{field_name}: {{{create_key_value_pair(value, operation)}}}'
+
+        return self
+
 
     def with_where_clauses(
         self, clauses: dict[str, str], overwrite: bool = False
--- tests/test_gql_dynamic_query_builder.py
+++ tests/test_gql_dynamic_query_builder.py
@@ -3,13 +3,53 @@ import pytest
 from src.api.gql_dynamic_query_builder import GQLDynamicQueryBuilder
 from tests.conftest import ALL_QUERIES
 
+class TestGQLDynamicQueryBuilder:
 
-@pytest.mark.parametrize("query,subquery_to_test", ALL_QUERIES)
-def test_build(query, subquery_to_test):
-    builder = GQLDynamicQueryBuilder(query)
+    @pytest.mark.parametrize("query,subquery_to_test", ALL_QUERIES)
+    def test_build_with_explicit_where_clauses(self, query, subquery_to_test):
+        builder = GQLDynamicQueryBuilder(query)
 
-    builder.with_where_clauses({'subquery_to_test': 'subquery_to_test_body_arg: {_eq : "test"}'})
-    result = builder.build()
+        builder.with_where_clauses({'subquery_to_test': 'subquery_to_test_body_arg: {_eq : "test"}'})
+        result = builder.build()
 
-    assert subquery_to_test not in result
-    assert 'subquery_to_test_body_arg: {_eq : "test"}' in result
+        assert subquery_to_test not in result
+        assert 'subquery_to_test_body_arg: {_eq : "test"}' in result
+
+
+    @pytest.mark.parametrize("query,subquery_to_test", ALL_QUERIES)
+    def test_build_with_api_where_clause_one_op_one_value(self, query, subquery_to_test):
+        builder = GQLDynamicQueryBuilder(query)
+
+        builder.with_where_clause('subquery_to_test', 'subquery_to_test_body_arg', 'test', '_eq')
+        result = builder.build()
+
+        assert subquery_to_test not in result
+        assert 'subquery_to_test_body_arg: {_eq: "test"}' in result
+
+
+    @pytest.mark.parametrize("query,subquery_to_test", ALL_QUERIES)
+    def test_build_with_api_where_clause_one_op_many_values(self, query, subquery_to_test):
+        builder = GQLDynamicQueryBuilder(query)
+        builder.with_where_clause('subquery_to_test', 'subquery_to_test_body_arg', ['test', 'hallo'], '_in')
+        result = builder.build()
+
+        assert subquery_to_test not in result
+        assert 'subquery_to_test_body_arg: {_in: ["test", "hallo"]}' in result
+
+
+    @pytest.mark.parametrize("query,subquery_to_test", ALL_QUERIES)
+    def test_build_with_api_where_clause_many_ops_one_value(self, query,subquery_to_test):
+        builder = GQLDynamicQueryBuilder(query)
+
+        with pytest.raises(TypeError):
+            builder.with_where_clause('subquery_to_test', 'subquery_to_test_body_arg', 'test', ['_in', '_eq'])
+
+
+    @pytest.mark.parametrize("query,subquery_to_test", ALL_QUERIES)
+    def test_build_with_api_where_clause_many_ops_many_values(self, query,subquery_to_test):
+        builder = GQLDynamicQueryBuilder(query)
+        builder.with_where_clause('subquery_to_test', 'subquery_to_test_body_arg', ['test', 'hallo'], ['_gte', '_lt'])
+        result = builder.build()
+
+        assert subquery_to_test not in result
+        assert 'subquery_to_test_body_arg: {_gte: "test" _lt: "hallo"}' in result
