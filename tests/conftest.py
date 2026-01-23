e64783a32d9bc9f9906d0e6349ca7cfa10b4d596
moritzsoelderer <moritzsoelderer@gmail.com>
2026-01-23 15:34:31 +0100

    test: replace main function in gql_dynamic_query_builder.py with test cases

---
+++ tests/conftest.py
@@ -0,0 +1,78 @@
+query_with_subquery_before_and_after = ("""
+        query Test {
+            test_subquery_before () {
+                test_subquery_body_arg_before
+            }
+            subquery_to_test () {
+                subquery_to_test_body_arg
+            }
+            test_subquery_after () {
+                test_subquery_body_arg_after
+            }
+        }
+    """,
+    """
+        subquery_to_test () {
+            subquery_to_test_body_arg
+        }
+    """)
+
+query_without_filters = ("""
+        query TestQuery {
+            subquery_to_test {
+                subquery_to_test_body_arg
+            }
+        }
+    """,
+    """
+        subquery_to_test {
+            subquery_to_test_body_arg
+        }
+    """)
+
+query_with_where = ("""
+        query TestQuery {
+            subquery_to_test (where: {producer: {_eq: "lego"}}) {
+                subquery_to_test_body_arg
+            }
+        }
+    """,
+    """
+        subquery_to_test (where: {producer: {_eq: "lego"}}) {
+            subquery_to_test_body_arg
+        }
+    """)
+
+query_with_limit = ("""
+        query TestQuery {
+            subquery_to_test (limit: 10) {
+                subquery_to_test_body_arg
+            }
+        }
+    """,
+    """
+        subquery_to_test (limit: 10) {
+            subquery_to_test_body_arg
+        }
+    """)
+
+query_with_where_and_limit = ("""
+        query TestQuery {
+            subquery_to_test (where: {producer: {_eq: "lego"}} limit: 10) {
+                subquery_to_test_body_arg
+            }
+        }
+    """,
+    """
+        subquery_to_test (where: {producer: {_eq: "lego"}} limit: 10) {
+            subquery_to_test_body_arg
+        }
+    """)
+
+ALL_QUERIES = [
+    query_with_where,
+    query_with_limit,
+    query_without_filters,
+    query_with_where_and_limit,
+    query_with_subquery_before_and_after
+]
---
+++ tests/test_gql_dynamic_query_builder.py
@@ -0,0 +1,15 @@
+import pytest
+
+from src.gql_dynamic_query_builder import GQLDynamicQueryBuilder
+from tests.conftest import ALL_QUERIES
+
+
+@pytest.mark.parametrize("query,subquery_to_test", ALL_QUERIES)
+def test_build(query, subquery_to_test):
+    builder = GQLDynamicQueryBuilder(query)
+
+    builder.with_where_clauses({'subquery_to_test': 'subquery_to_test_body_arg: {_eq : "test"}'})
+    result = builder.build()
+
+    assert subquery_to_test not in result
+    assert 'subquery_to_test_body_arg: {_eq : "test"}' in result
