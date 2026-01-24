5c8aef3944d2075eb84f414a2fc08257f7bdae3c
moritzsoelderer <moritzsoelderer@gmail.com>
2026-01-24 15:18:07 +0100

    feat: implement support for nested fields

--- src/api/gql_dynamic_query_builder.py
+++ src/api/gql_dynamic_query_builder.py
@@ -13,28 +13,54 @@ def create_key_value_pair(value: str | int | list, operation: str) -> str:
 
     return f'{operation}: {value}'
 
+def construct_where_clause_string(nested_anc_explicit_where_clauses: dict) -> str:
+    opt_explicit_clause = nested_anc_explicit_where_clauses.get('explicit_clause', None)
+    nested_field_clauses = [opt_explicit_clause] if opt_explicit_clause else []
+    for key, value in nested_anc_explicit_where_clauses.items():
+        if isinstance(value, dict):
+            nested_field_clauses.append(f"{key}: {{{construct_where_clause_string(value)}}}")
+        else:
+            nested_field_clauses.append(value)
+
+    return " ".join(nested_field_clauses)
 
 class GQLDynamicQueryBuilder:
     def __init__(self, query: str):
         self.query: str = query
-        self.where_clauses: dict[str, str] = {}
+        self.where_clauses: dict[str, dict[str, dict | str]] = {}
         self.processed_query = query
 
+    def update_where_clauses(self, table_name: str, field_name: str, clause: str) -> None:
+        current_table_clauses = self.where_clauses[table_name] if self.where_clauses.get(table_name, None) else {}
+        fields = field_name.split('.')
+
+        clause_dict = {fields[-1]: f'{fields[-1]}: {clause}'}
+        for field in fields[:-1]:
+            clause_dict = {field: clause_dict}
+
+        current_table_clauses.update(clause_dict)
+        self.where_clauses[table_name] = current_table_clauses
+
     def with_where_clause(
-        self, table_name: str, field_name: str, value: str | int | list[int] | list[str], operation: str | list[str]
+        self, table_name: str, field_name: str, value: str | int | list[int] | list[str], operation: str | list[str], skip_if_none:bool = False
     ):
+        if value is None:
+            if skip_if_none:
+                return self
+            else:
+                raise ValueError("Encountered None value in with_where_clause - if you want to skip it set skip_if_none=True")
+
         if isinstance(value, list):
             if isinstance(operation, list):
                 pairs = [create_key_value_pair(v, o) for v, o in zip(value, operation, strict=True)]
-                self.where_clauses[table_name] = f'{field_name}: {{{" ".join(pairs)}}}'
+                self.update_where_clauses(table_name, field_name, f'{{{" ".join(pairs)}}}')
             else:
                 value = json.dumps(value)
-                self.where_clauses[table_name] = f'{field_name}: {{{operation}: {value}}}'
+                self.update_where_clauses(table_name, field_name, f'{{{operation}: {value}}}')
         else:
             if isinstance(operation, list):
                 raise TypeError("Operation should be scalar if value is scalar")
-
-            self.where_clauses[table_name] = f'{field_name}: {{{create_key_value_pair(value, operation)}}}'
+            self.update_where_clauses(table_name, field_name, f'{{{create_key_value_pair(value, operation)}}}')
 
         return self
 
@@ -43,13 +69,19 @@ class GQLDynamicQueryBuilder:
         self, clauses: dict[str, str], overwrite: bool = False
     ) -> GQLDynamicQueryBuilder:
         if overwrite:
-            self.where_clauses = dict(clauses)
+            transformed_clauses = {table_name: {'explicit_clause': clause} for table_name, clause in clauses.items()}
+            self.where_clauses = transformed_clauses
         else:
-            self.where_clauses.update(clauses)
+            for table_name, clause in clauses.items():
+                if self.where_clauses.get(table_name, None):
+                    self.where_clauses[table_name].update({'explicit_clause': clause})
+                else:
+                    self.where_clauses.update({table_name: {'explicit_clause': clause}})
         return self
 
     def build(self):
-        for table_name, where_clauses in self.where_clauses.items():
+        for table_name, nested_and_explicit_where_clauses in self.where_clauses.items():
+            where_clauses = construct_where_clause_string(nested_and_explicit_where_clauses)
             query_grammar = prepare_query_grammar(table_name, where_clauses)
             self.processed_query = query_grammar.transform_string(self.processed_query)
             print('Processed Query', self.processed_query)
